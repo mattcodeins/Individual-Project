a0e2a92d7d717be2914dd7bd29c703c7dffe19c0
USER <user@dyn3184-14.wlan.ic.ac.uk>
2022-05-10 14:35:31 +0100

    gp

---
+++ gaussian_process/main.py
@@ -0,0 +1,13 @@
+"""
+import MNIST
+import GPFLOW
+
+preform regression using gaussian process
+
+Tasks: 1) Train gaussian process to compute marginal likelihood on MNIST.
+
+Use GPFLOW, toy regression using variational gaussian process approximation (SGPR, SU(V?)GP).
+
+
+
+"""
