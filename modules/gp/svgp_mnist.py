82f1f631dc0d6c115c1b74baf10d8d8f8b919a0d
USER <user@dyn3188-235.wlan.ic.ac.uk>
2022-06-15 11:54:34 +0100

    clean up gp

--- modules/gp/svgp_mnist.py
+++ modules/gp/svgp_mnist.py
@@ -1,10 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import gpflow
-from typing import Dict, Optional, Tuple
-from gpflow.utilities import to_default_float, set_trainable
+from typing import Dict
+from gpflow.utilities import to_default_float
 
 
 original_dataset, info = tfds.load(name="mnist", split=tfds.Split.TRAIN, with_info=True)
@@ -13,11 +13,10 @@ image_shape = info.features["image"].shape
 image_size = tf.reduce_prod(image_shape)
 batch_size = 200
 
-
 def map_fn(input_slice: Dict[str, tf.Tensor]):
     updated = input_slice
     image = to_default_float(updated["image"]) / 255.0
-    label = to_default_float(updated["label"])
+    label = tf.one_hot(int(updated["label"]), 10, dtype='int32')
     return tf.reshape(image, [-1, image_size]), label
 
 
@@ -36,7 +35,7 @@ images_subset, _ = next(iter(dataset))
 images_subset = tf.reshape(images_subset, [-1, image_size])
 
 kernel = gpflow.kernels.SquaredExponential()
-likelihood = gpflow.likelihoods.MultiClass(num_mnist_classes)
+likelihood = gpflow.likelihoods.Softmax(num_mnist_classes)
 Z = images_subset.numpy()[:num_inducing_points, :]
 
 model = gpflow.models.SVGP(
@@ -59,19 +58,25 @@ test_dataset = (
 )
 test_images, test_labels = next(iter(test_dataset))
 
-
-
 data_iterator = iter(dataset)
 
 training_loss = model.training_loss_closure(data_iterator)
 
 logf = []
-optimizer = tf.optimizers.Adam(learning_rate=1e-4)
+lr_schedule = tf.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate=1e-2,
+    decay_steps=1000000,
+    decay_rate=0.9)
+optimizer = tf.optimizers.Adam(learning_rate=lr_schedule)
+
 
 @tf.function
 def optimization_step():
     optimizer.minimize(training_loss, model.trainable_variables)
 
+
+f = open("training_results.txt", "w")
+f.write("elbo accuracy variance lengthscale\n")
 print("Starting optimisation...")
 for step in range(1000000):
     optimization_step()
@@ -80,8 +85,15 @@ for step in range(1000000):
         logf.append(elbo)
 
         m, v = model.predict_y(test_images)
-        preds = np.argmax(m, 1).reshape(test_labels.numpy().shape)
-        correct = preds == test_labels.numpy().astype(int)
-        acc = np.average(correct.astype(float)) * 100.0
+        preds = tf.one_hot(np.argmax(m, 1), 10, dtype='int32')
+        correct = preds == test_labels.numpy()
+        acc = np.average(correct) * 100.0
 
         print("ELBO: {:.4e}, Accuracy is {:.4f}%".format(elbo, acc))
+        f.write("{:.4e} {:.4f} {:.4f} {:.4f}\n".format(
+            elbo,
+            acc,
+            model.kernel.variance.numpy(),
+            model.kernel.lengthscales.numpy())
+        )
+f.close()
