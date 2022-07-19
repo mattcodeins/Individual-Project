83c800d7c099913ba0281bd90acdb0b0b7eb91dc
USER <user@users-mbp.home>
2022-07-19 12:29:30 +0100

    regession update

--- bayes_approx/old_experiments/bnn_toy_regression.py
+++ bayes_approx/old_experiments/bnn_toy_regression.py
@@ -3,7 +3,14 @@ import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
 
-import bayes_approx.datasets.toy_regression as d
+import os
+import sys
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from datasets import toy_regression as d
 from modules.bnn.modules.linear import make_linear_bnn
 from modules.bnn.modules.loss import GaussianKLLoss, nELBO
 from modules.bnn.utils import *
@@ -38,11 +45,11 @@ if __name__ == "__main__":
 
     # create dataset
     N_data = 100; noise_std = 0.1
-    train_loader, test_loader, train_data, test_data = d.create_regression_dataset(N_data, noise_std)
+    train_loader, test_loader, normal_train, test = d.create_regression_dataset(N_data, noise_std)
 
     # create bnn
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-    x_dim, y_dim = train_data.x.shape[1], train_data.y.shape[1]
+    x_dim, y_dim = normal_train.x.shape[1], normal_train.y.shape[1]
     h_dim = 50
     layer_sizes = [x_dim, h_dim, h_dim, y_dim]
     activation = nn.GELU()
@@ -55,19 +62,8 @@ if __name__ == "__main__":
     log_noise_var = torch.ones(size=(), device=device)*-3.0  # Gaussian likelihood
     print("BNN architecture: \n", model)
 
-    d.plot_bnn_prior(model, predict, train_data, test_data, log_noise_var, noise_std, device)
-
-    # # plot the BNN prior in function space
-    # K = 50  # number of Monte Carlos samples used in test time
-    # x_test_norm = d.normalise_data(test_data.x, train_data.x_mean, train_data.x_std)
-    # x_test_norm = torch.tensor(x_test_norm, ).float().to(device)
-
-    # y_pred_mean, y_pred_std_noiseless = d.get_regression_results(model, x_test_norm, K, predict, train_data)
-    # model_noise_std = d.unnormalise_data(to_numpy(torch.exp(0.5*log_noise_var)), 0.0, train_data.y_std)
-    # y_pred_std = np.sqrt(y_pred_std_noiseless ** 2 + model_noise_std ** 2)
-    # d.plot_regression(train_data.x, train_data.y, test_data.x, test_data.y, y_pred_mean, y_pred_std_noiseless, y_pred_std,
-    #                   title='BNN init (before training, MFVI)')
-    # print(model_noise_std, noise_std, y_pred_std_noiseless.mean())
+    d.plot_bnn_pred_post(model, predict, normal_train, test, log_noise_var, noise_std,
+                         'BNN init (before training, MFVI)', device)
 
     # training hyperparameters
     learning_rate = 1e-4
@@ -85,7 +81,7 @@ if __name__ == "__main__":
     logs = []
     for i in range(N_epochs):
         loss, nll, kl = train_step(
-            model, opt, nelbo, dataloader, log_noise_var, device=device
+            model, opt, nelbo, train_loader, log_noise_var, device=device
         )
         logs.append([to_numpy(nll), to_numpy(kl), to_numpy(loss), to_numpy(nll)/to_numpy(kl)])
         if (i+1) % 100 == 0:
@@ -93,22 +89,7 @@ if __name__ == "__main__":
                   .format(i+1, logs[-1][0], logs[-1][1], logs[-1][2], logs[-1][3]))
     logs = np.array(logs)
 
-    # plot the training curve
-    def plot_training_loss(logs):
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
-        ax1.plot(np.arange(logs.shape[0]), logs[:, 0], 'r-')
-        ax2.plot(np.arange(logs.shape[0]), logs[:, 1], 'r-')
-        ax1.set_xlabel('epoch')
-        ax2.set_xlabel('epoch')
-        ax1.set_title('nll')
-        ax2.set_title('kl')
-        plt.show()
+    d.plot_training_loss(logs)
 
-    plot_training_loss(logs)
-
-    y_pred_mean, y_pred_std_noiseless = d.get_regression_results(model, x_test_norm, K, predict, dataset)
-    model_noise_std = d.unnormalise_data(to_numpy(torch.exp(0.5*log_noise_var)), 0.0, dataset.y_std)
-    y_pred_std = np.sqrt(y_pred_std_noiseless ** 2 + model_noise_std ** 2)
-    d.plot_regression(x_train, y_train, x_test, y_test, y_pred_mean, y_pred_std_noiseless, y_pred_std,
-                      title='BNN approx. posterior (MFVI)')
-    print(model_noise_std, noise_std, y_pred_std_noiseless.mean())
+    d.plot_bnn_pred_post(model, predict, normal_train, test, log_noise_var, noise_std,
+                         'BNN approx. posterior (MFVI)', device)
